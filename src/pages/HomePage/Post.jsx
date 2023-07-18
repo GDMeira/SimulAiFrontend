7717ac166b83a452246a2b7f5ee04418fff84e5d
GDMeira <gabrieldega970@gmail.com>
2023-07-18 01:47:04 -0300

    fix: corrigida a responsividade da tela home.

--- src/pages/HomePage/Post.jsx
+++ src/pages/HomePage/Post.jsx
@@ -47,7 +47,7 @@ export default function Post({ position, post, colors }) {
 
 const PostTextSC = styled.div`
     width: 100%;
-    height: 50vh;
+    height: 35vh;
     display: flex;
     justify-content: space-evenly;
     align-items: center;
@@ -63,25 +63,37 @@ const PostTextSC = styled.div`
 
         h1 {
             font-size: 0.9em;
-            margin: 2em;
+            margin-top: 3vh;
+            margin-bottom: 1em;
             text-align: center;
         }
 
         p {
             font-size: 0.6em;
-            line-height: 2.5em;
+            line-height: 1.2em;
             text-align: left;
         }
     }
 
     img {
-        width: 40vw;
+        height: 33vh;
+    }
+
+    @media (max-width: 1200px) {
+        height: 30vh;
+        img {
+            height: 20vh;
+        }
+
+        div {
+            width: 60%;
+        }
     }
 `;
 
 const PostButtonSC = styled.div`
     width: 100%;
-    height: 50vh;
+    height: 35vh;
     display: flex;
     flex-direction: column;
     justify-content: space-evenly;
@@ -89,16 +101,19 @@ const PostButtonSC = styled.div`
     background-color: ${props => props.color.background};
 
     h1 {
-        font-size: 1.5em;
+        font-size: 1.2em;
     }
 
     p {
-        font-size: 0.7em;
+        font-size: 0.6em;
+        padding-left: 1%;
     }
 
     button {
-        width: 60%;
-        height: 11vw; 
+        width: 30%;
+        height: 7vw; 
+        min-width: 200px;
+        min-height: 60px;
         background-color: ${props => props.color.button}; 
         color: #fff;
         font-size: 0.7em;
